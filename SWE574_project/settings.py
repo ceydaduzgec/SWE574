cc09e0232b4029ff4c0127a0f102a96e88dffe32
Ceyda Duzgec <cduzgec@gmail.com>
2023-03-28 23:33:01 +0300

    Change BigAutoField to AutoField

--- SWE574_project/settings.py
+++ SWE574_project/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "users/static"),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 MEDIA_URL = "/images/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "posts/static/images")
