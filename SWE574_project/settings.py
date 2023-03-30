bd012637cf26cbe19e58fb86b114dcbc32d0c9ab
Ceyda Duzgec <cduzgec@gmail.com>
2023-03-30 23:03:44 +0300

    Delete multiplication

--- SWE574_project/settings.py
+++ SWE574_project/settings.py
@@ -61,9 +61,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-if DEBUG:
-    INSTALLED_APPS.append("django_extensions")
-
 ROOT_URLCONF = "SWE574_project.urls"
 
 TEMPLATES = [
