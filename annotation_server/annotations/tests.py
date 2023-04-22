0ab281cbd9b6548666d1a6120bbb6dfe86376373
Ceyda Duzgec <cduzgec@gmail.com>
2023-04-22 18:56:56 +0300

    Start on front end of annotation

--- annotation_server/annotations/tests.py
+++ annotation_server/annotations/tests.py
@@ -1,27 +1,63 @@
+from annotations.models import Annotation
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-from .models import Annotation
 
-
-class AnnotationModelTest(TestCase):
-    def test_as_dict_method(self):
-        annotation = Annotation(
-            id="http://example.org/anno1",
+class AnnotationViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.annotation1 = Annotation.objects.create(
+            id="test1",
+            context="http://www.w3.org/ns/anno.jsonld",
             type="Annotation",
-            body={"type": "TextualBody", "value": "Test annotation"},
-            target={
-                "source": "http://example.org/document1",
-                "selector": {"type": "FragmentSelector", "value": "page=2"},
-            },
+            body="http://example.com/1",
+            target="http://example.com/2",
         )
-        expected_dict = {
-            "@context": "http://www.w3.org/ns/anno.jsonld",
-            "id": "http://example.org/anno1",
+        self.annotation2 = Annotation.objects.create(
+            id="test2",
+            context="http://www.w3.org/ns/anno.jsonld",
+            type="Annotation",
+            body="http://example.com/3",
+            target="http://example.com/4",
+        )
+
+    def test_create_annotation(self):
+        url = reverse("annotations")
+        data = {
+            "id": "test3",
+            "context": "http://www.w3.org/ns/anno.jsonld",
             "type": "Annotation",
-            "body": {"type": "TextualBody", "value": "Test annotation"},
-            "target": {
-                "source": "http://example.org/document1",
-                "selector": {"type": "FragmentSelector", "value": "page=2"},
-            },
+            "body": "http://example.com/5",
+            "target": "http://example.com/6",
         }
-        self.assertEqual(annotation.as_dict(), expected_dict)
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Annotation.objects.count(), 3)
+        self.assertEqual(Annotation.objects.get(id="test3").body, "http://example.com/5")
+
+    def test_get_annotation(self):
+        url = reverse("annotations")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_get_single_annotation(self):
+        url = reverse("annotations", args=[self.annotation1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["body"], "http://example.com/1")
+
+    def test_update_annotation(self):
+        url = reverse("annotations", args=[self.annotation1.id])
+        data = {"body": "http://example.com/updated"}
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Annotation.objects.get(id="test1").body, "http://example.com/updated")
+
+    def test_delete_annotation(self):
+        url = reverse("annotations", args=[self.annotation1.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Annotation.objects.count(), 1)
