ff9968a8b3c0119c9e3b6b8770b223f3ed150748
Ceyda Duzgec <cduzgec@gmail.com>
2023-05-05 20:52:45 +0300

    Alter Annotation model

--- annotation_server/annotations/models.py
+++ annotation_server/annotations/models.py
@@ -7,8 +7,8 @@ class Annotation(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     context = models.CharField(max_length=255, default="http://www.w3.org/ns/anno.jsonld", validators=[validate_iri])
     type = models.CharField(max_length=255, default="Annotation", validators=[validate_annotation_type])
-    body = models.CharField(max_length=225, validators=[validate_iri])
-    target = models.CharField(max_length=225, validators=[validate_iri])
+    body = models.JSONField(max_length=255)
+    target = models.JSONField(max_length=255)
     creation_datetime = models.DateTimeField(auto_now_add=True)
 
     class Meta:
