95527e8c705a1c90809772aa48689dc49ce4d028
Ceyda Duzgec <cduzgec@gmail.com>
2023-05-05 21:12:54 +0300

    Add AnnotationSearchTestCase

--- annotation_server/annotations/tests.py
+++ annotation_server/annotations/tests.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from .serializers import AnnotationSerializer
+
 
 class AnnotationViewTestCase(TestCase):
     def setUp(self):
@@ -54,3 +56,33 @@ class AnnotationViewTestCase(TestCase):
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Annotation.objects.count(), 1)
+
+
+class AnnotationSearchTestCase(TestCase):
+    def setUp(self):
+        self.annotation1 = Annotation.objects.create(
+            id="http://example.org/anno1",
+            body={"type": "TextualBody", "value": "This is the first annotation."},
+            target={"source": "http://example.com/page1", "selector": {"type": "CssSelector", "value": ".section1"}},
+        )
+        self.annotation2 = Annotation.objects.create(
+            id="http://example.org/anno2",
+            body={"type": "TextualBody", "value": "This is the second annotation."},
+            target={"source": "http://example.com/page1", "selector": {"type": "CssSelector", "value": ".section2"}},
+        )
+
+    def test_search_annotation_with_query(self):
+        url = reverse("annotation_search")
+        response = self.client.get(url, {"query": "second"})
+        self.assertEqual(response.status_code, 200)
+
+        serializer = AnnotationSerializer([self.annotation2], many=True)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_search_annotation_without_query(self):
+        url = reverse("annotation_search")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 400)
+
+        expected_data = {"error": "Missing query parameter"}
+        self.assertEqual(response.json(), expected_data)
